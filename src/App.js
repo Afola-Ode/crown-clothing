e8e796213ba7fed822e75c1e707af3f4afb946a9
Afola-Ode <36237399+Afola-Ode@users.noreply.github.com>
2021-11-02 17:57:18 +0100

    updating the app with ashop page, collection preview page and collection item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import Shop from './pages/Shop/Shop';
+
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path ="/" component = {Home} />
+        <Route path ="/shop" component = {Shop} />
+      </Switch>
+    </Router>
   );
 }
 
